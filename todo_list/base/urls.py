4694b0ed9ead2d5a5e027981b15b7d28899544b1
KaloyankerR <kaloyankulov2003kk@gmail.com>
2023-01-12 23:50:36 +0100

    Created Create task page and logic

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TaskList, TaskDetail
+from .views import TaskList, TaskDetail, TaskCreate
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
-    path('task/<int:pk>/', TaskDetail.as_view(), name='task')
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('task-create/', TaskCreate.as_view(), name='task-create'),
 ]
--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 from .models import Task
 
 
@@ -14,3 +15,8 @@ class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'base/task.html'
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
