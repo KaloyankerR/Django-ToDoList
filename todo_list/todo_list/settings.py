c93ef6d8c721e7b51e93aabe6bda27a07160b4be
KaloyankerR <kaloyankulov2003kk@gmail.com>
2023-01-12 23:22:55 +0100

    Task list page created

--- todo_list/todo_list/settings.py
+++ todo_list/todo_list/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'todo_list.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['base/templates/base'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
