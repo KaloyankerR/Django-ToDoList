32abe487815ed5fe166abb553b2ddaa46f5629dc
KaloyankerR <kaloyankulov2003kk@gmail.com>
2023-01-16 13:40:03 +0100

    Search functionality implemented

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -37,6 +37,7 @@ class RegisterPage(FormView):
             return redirect('tasks')
         return super(RegisterPage, self).get()
 
+
 class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'
@@ -45,6 +46,13 @@ class TaskList(LoginRequiredMixin, ListView):
         context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(user=self.request.user)
         context['count'] = context['tasks'].filter(complete=False).count()
+
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['tasks'] = context['tasks'].filter(title__icontains=search_input)
+        
+        context['search_input'] = search_input
+
         return context
 
 
